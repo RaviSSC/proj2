906c8cac9c626e9053a2e89a3432db3729a30fc2
Nayah <ravichahal@gmail.com>
2022-12-08 04:27:14 -0700

    updating

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/movies', {
+mongoose.connect('mongodb://localhost/Project2', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
